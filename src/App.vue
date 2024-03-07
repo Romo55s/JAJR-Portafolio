28330aba66fb4fca4b2955fb1e69ad5ed2cd0ef0
Tony <pp.tony777@gmail.com>
2024-03-06 23:07:20 -0600

    fixing the deploy issue

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,13 @@ import Resume from "@/views/Resume.vue";
 import Portfolio from "@/views/Portfolio.vue";
 import About from "@/views/About.vue";
 import Footer from "./components/Footer.vue";
-import { toast } from "vue3-toastify";
-import "vue3-toastify/dist/index.css";
+import { toast } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
 
 onMounted(() => {
-  toast.warn("Work in progress", {
-    position: toast.POSITION.TOP_RIGHT,
-    theme: "dark",
-    duration: 2000,
+  toast.warn("Warning Notification !", {
+    position: toast.POSITION.BOTTOM_LEFT,
+    theme: 'dark',
   });
 });
 </script>
